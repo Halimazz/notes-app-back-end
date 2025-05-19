deb8f097334dbbb772b6a09de6e56c195ce6fa4a
Halimazz <85574466+Halimazz@users.noreply.github.com>
2025-05-19 23:22:43 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes"); // Mengimpor rute dari routes.js
+
+const app = express();
+
+// Middleware untuk body parsing
+app.use(express.json()); // Untuk meng-handle JSON payload
+
+// Middleware CORS untuk semua origin
+app.use(cors()); // Middleware CORS yang diaktifkan untuk semua origin
+
+// Menggunakan rute yang telah didefinisikan di routes.js
+routes(app); // Menyediakan instance 'app' ke dalam routes
+
+// Konfigurasi host
+const host = process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0";
+
+// Menjalankan server pada port 5001
+app.listen(5000, () => {
+  console.log(`Server berjalan pada http://${host}:${5000}`);
+});
 
