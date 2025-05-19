0ac123e3142a015ae1c22eae0f0fe5b1ebcac01f
kachponzchi <sanankciez@gmail.com>
2025-05-19 21:25:24 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -14,7 +14,10 @@ app.use(cors()); // Middleware CORS yang diaktifkan untuk semua origin
 // Menggunakan rute yang telah didefinisikan di routes.js
 routes(app); // Menyediakan instance 'app' ke dalam routes
 
+// Konfigurasi host
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+
 // Menjalankan server pada port 5001
 app.listen(5002, () => {
-  console.log("Server berjalan pada http://localhost:5002");
+  console.log(`Server berjalan pada http://${host}:${5002}`);
 });
